de4295afee3c02c839a1e16f635719769286924a
Tchy258 <tchy258@yahoo.cl>
2025-07-13 19:19:37 -0400

    feat: add size setters and triangle getter to concept, docs

--- include/concepts/mesh_data.hpp
+++ include/concepts/mesh_data.hpp
@@ -6,6 +6,18 @@
 #include<unordered_map>
 #include<mesh_data/structures/vertex.hpp>
 
+/**
+ * All meshes used throughout the program as template arguments must adhere to this concept, so a mesh must:
+ * - Define a type for its vertices and edges, `Mesh::VertexType` and `Mesh::EdgeType`
+ * - Define a type for the indices of its vertices and edges, `Mesh::VertexIndex` and `Mesh::EdgeIndex`
+ * - Have a vector of vertices, a vector of edges and a vector of ints representing polygons
+ * - Have getters for vertices, edges and polygons as well as size getters and setters
+ * - Have a way to retrieve the vertices of a triangle at a given face
+ * - Have a method to retrieve the indices of the neighbors of a triangle
+ * - Be constructible from a vector of vertices, edges and faces
+ * - Be copy constructible
+ * - Have vertices that are at least a type of `Vertex`
+ */
 template<typename Mesh>
 concept MeshData = requires(
     Mesh& mesh,
@@ -15,8 +27,7 @@ concept MeshData = requires(
     typename Mesh::VertexType vertexT,
     typename Mesh::EdgeType edgeT,
     typename Mesh::VertexIndex,
-    typename Mesh::EdgeIndex,
-    std::unordered_map<int, std::vector<typename Mesh::EdgeIndex>> cavityMap 
+    typename Mesh::EdgeIndex
 ) {
     { mesh.getVertex(1) } -> std::same_as<typename Mesh::VertexType&>;
     { mesh.getEdge(1) } -> std::same_as<typename Mesh::EdgeType&>;
@@ -24,8 +35,11 @@ concept MeshData = requires(
     { mesh.numberOfVertices()} -> std::same_as<size_t>;
     { mesh.numberOfEdges()} -> std::same_as<size_t>;
     { mesh.numberOfPolygons()} -> std::same_as<size_t>;
+    { mesh.updatePolygonCount(1)} -> std::same_as<void>;
+    { mesh.updateVertexCount(1)} -> std::same_as<void>;
+    { mesh.updateEdgeCount(1)} -> std::same_as<void>;
     { mesh.getNeighbors(1)} -> std::same_as<std::vector<int>>;
-    { mesh.cavityInsertion(cavityMap)} -> std::same_as<void>;
+    { mesh.getVerticesOfTriangle(1,vertexT, vertexT, vertexT)} -> std::same_as<void>;
     Mesh(vertexVec, edgeVec, faceVec);
     Mesh(mesh);
 } &&
