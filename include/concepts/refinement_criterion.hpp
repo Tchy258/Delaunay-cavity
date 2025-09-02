985129cd5be1281f65521f6fcf4a1cfad863a48a
Tchy258 <tchy258@yahoo.cl>
2025-09-02 01:32:00 -0400

    fix: The Mesh typename must be a type of MeshData

--- include/concepts/refinement_criterion.hpp
+++ include/concepts/refinement_criterion.hpp
@@ -1,11 +1,17 @@
 #ifndef REFINEMENT_CRITERION_HPP
 #define REFINEMENT_CRITERION_HPP
 #include<concepts>
+#include<concepts/mesh_data.hpp>
 
+/**
+ * A refinement criterion must apply to a particular mesh that conforms to the MeshData concept,
+ * and it must have an operator() that receives an input mesh, a polygon index, and returns true or false
+ */
 template <typename Criterion, typename Mesh>
 concept RefinementCriterion = requires(Criterion criterion, Mesh inputMesh, int polygonIndex) {
     {criterion(inputMesh,polygonIndex)} -> std::convertible_to<bool>;
-};
+} &&
+MeshData<Mesh>;
 
 
 #endif
