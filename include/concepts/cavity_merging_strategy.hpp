5b0442513c9db105582a6cd21c4ec73540ca7fb3
Tchy258 <tchy258@yahoo.cl>
2025-09-24 16:07:07 -0300

    feat: cavity merging strategy concept to determine how to propagate

---
+++ include/concepts/cavity_merging_strategy.hpp
@@ -0,0 +1,28 @@
+#ifndef CAVITY_MERGING_STRATEGY
+#define CAVITY_MERGING_STRATEGY
+#include<concepts>
+#include<concepts/mesh_data.hpp>
+#include<vector>
+#include<mesh_refiners/delaunay_cavity/helper_structs/cavity.hpp>
+
+template <typename MergingStrategy, typename Mesh>
+concept HasPreAddMethod = requires(Mesh* mesh, 
+    const std::vector<std::pair<typename Mesh::VertexType, typename Mesh::FaceIndex>>& circumcenters,
+    typename Mesh::FaceIndex triangle) {
+        { MergingStrategy::preAdd(mesh, triangle, circumcenters)} -> std::convertible_to<bool>;
+} && MeshData<Mesh>;
+
+template <typename MergingStrategy, typename Mesh>
+concept HasPostComputeMethod = requires(Mesh* mesh,
+    std::vector<refiners::helpers::delaunay_cavity::Cavity<Mesh>>& cavities) {
+        { MergingStrategy::postCompute(mesh, cavities)} -> std::same_as<void>;
+} && MeshData<Mesh>;
+
+/**
+ * A cavity merging strategy must apply to a particular mesh that conforms to the MeshData concept,
+ * and it must have either a preAdd method or postCompute method
+ */
+template <typename MergingStrategy, typename Mesh>
+concept CavityMergingStrategy = HasPreAddMethod<MergingStrategy,Mesh> || HasPostComputeMethod<MergingStrategy,Mesh>;
+
+#endif
