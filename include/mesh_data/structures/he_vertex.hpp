cb0986a43de48eb7e9445c06494203183774f73a
Tchy258 <tchy258@yahoo.cl>
2025-09-24 16:10:26 -0300

    feat: static assert for compliance with concept

--- include/mesh_data/structures/he_vertex.hpp
+++ include/mesh_data/structures/he_vertex.hpp
@@ -1,10 +1,12 @@
 #ifndef HE_VERTEX_HPP
 #define HE_VERTEX_HPP
 #include <mesh_data/structures/vertex.hpp>
-
+#include <concepts/is_half_edge_vertex.hpp>
 struct HEVertex : public Vertex {
     int incidentHalfEdge;
     bool isBorder;
+
 };
+static_assert(IsHalfEdgeVertex<HEVertex>);
 
 #endif
