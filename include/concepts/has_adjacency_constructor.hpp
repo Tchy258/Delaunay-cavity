2aedca60f18b5f6723fbd21b301b93b462e7d576
Tchy258 <tchy258@yahoo.cl>
2025-09-24 16:07:32 -0300

    refactor: rewritten to be more clear on what it means and does

--- include/concepts/has_adjacency_constructor.hpp
+++ include/concepts/has_adjacency_constructor.hpp
@@ -1,14 +1,29 @@
 #ifndef HAS_ADJACENCY_CONSTRUCTOR_HPP
 #define HAS_ADJACENCY_CONSTRUCTOR_HPP
-#include<concepts>
-#include<vector>
+#include <concepts>
+#include <concepts/primitive_integral.hpp>
+#include <concepts/mesh_data.hpp>
+#include <vector>
 
+
+/**
+ * A Mesh type has an adjacency constructor if it can be constructed from a vector of vertices, edges, faces and neighbor faces
+ */
 template<typename Mesh>
 concept HasAdjacencyConstructor =
-    std::constructible_from<Mesh,
-        std::vector<typename Mesh::VertexType>,
-        std::vector<typename Mesh::EdgeType>,
-        std::vector<int>, 
-        std::vector<int>>;
+    requires (
+        std::vector<typename Mesh::VertexType> vertices,
+        std::vector<typename Mesh::EdgeType> edges,
+        std::vector<typename Mesh::FaceIndex> faces,
+        std::vector<typename Mesh::FaceIndex> neighbors
+    ) {
+        Mesh(
+            vertices,
+            edges,
+            faces,
+            neighbors
+        );
+    }
+    && MeshData<Mesh>;
 
 #endif
