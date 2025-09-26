0cae323ee0c3d1ab54aaef818ddb864e15c3d2cc
Tchy258 <tchy258@yahoo.cl>
2025-09-25 23:32:25 -0300

    refactor: move polylla's containers to a separate data class to avoid circular dependencies

---
+++ include/mesh_refiners/polylla/helper_structs/polylla_data.hpp
@@ -0,0 +1,28 @@
+#ifndef POLYLLA_DATA_HPP
+#define POLYLLA_DATA_HPP
+
+#include <misc/mesh_stat.hpp>
+#include <misc/time_stat.hpp>
+#include <unordered_map>
+#include <vector>
+#include <concepts/mesh_data.hpp>
+
+template <MeshData MeshType>
+struct PolyllaData {
+    using OutputIndex = typename MeshType::OutputIndex;
+    using BinaryVector = std::vector<uint8_t>;
+
+    std::unordered_map<MeshStat, int> meshStats;
+    std::unordered_map<TimeStat, double> timeStats;
+    std::vector<OutputIndex> outputSeeds; //Seeds of the polygon
+
+    //std::vector<int> triangles; //True if the edge generated a triangle CHANGE!!!!
+
+    BinaryVector maxEdges; //True if the edge i is a max edge
+    BinaryVector frontierEdges; //True if the edge i is a frontier edge
+    std::vector<OutputIndex> seedCandidates; //Seed edges that generate polygon simple and non-simple
+
+    BinaryVector seedBarrierEdgeTipMark;
+};
+
+#endif // POLYLLA_DATA_HPP
--- include/mesh_refiners/polylla/polylla_refiner.hpp
+++ include/mesh_refiners/polylla/polylla_refiner.hpp
@@ -3,6 +3,7 @@
 #include<concepts/mesh_data.hpp>
 #include<mesh_refiners/mesh_refiner.hpp>
 #include<mesh_refiners/polylla/mesh_helpers/mesh_helper_polylla.hpp>
+#include<mesh_refiners/polylla/helper_structs/polylla_data.hpp>
 #include<misc/mesh_stat.hpp>
 #include<misc/time_stat.hpp>
 
@@ -16,46 +17,39 @@ class PolyllaRefiner : public MeshRefiner<MeshType> {
         using FaceIndex = typename MeshType::FaceIndex;
         using OutputIndex = typename MeshType::OutputIndex;
     private:
-        using _polygon = std::vector<int>; 
-        using BinaryVector = std::vector<uint8_t>;
         using _MeshHelper = refiners::helpers::polylla::MeshHelper<MeshType>;
 
-        std::unordered_map<MeshStat, int> meshStats;
-        std::unordered_map<TimeStat, double> timeStats;
-        std::vector<OutputIndex> outputSeeds; //Seeds of the polygon
+        PolyllaData<MeshType> data;
 
-        //std::vector<int> triangles; //True if the edge generated a triangle CHANGE!!!!
-
-        BinaryVector maxEdges; //True if the edge i is a max edge
-        BinaryVector frontierEdges; //True if the edge i is a frontier edge
-        std::vector<OutputIndex> seedOutputs; //Seed edges that generate polygon simple and non-simple
-
-        // Auxiliary array used during the barrier-edge elimination
-        std::vector<FaceIndex> triangleList;
-        BinaryVector seed_bet_mark;
-
-        friend class refiners::helpers::polylla::MeshHelper<MeshType>;
     public:
         PolyllaRefiner() {
-            meshStats[N_POLYGONS] = 0;
-            meshStats[N_FRONTIER_EDGES] = 0;
-            meshStats[N_BARRIER_EDGE_TIPS] = 0;
-            meshStats[N_POLYGONS_TO_REPAIR] = 0;
-            meshStats[N_POLYGONS_ADDED_AFTER_REPAIR] = 0;
-            meshStats[N_VERTICES] = 0;
-            meshStats[N_EDGES] = 0;
-            meshStats[N_BORDER_EDGES] = 0;
+            data.meshStats[N_POLYGONS] = 0;
+            data.meshStats[N_FRONTIER_EDGES] = 0;
+            data.meshStats[N_BARRIER_EDGE_TIPS] = 0;
+            data.meshStats[N_POLYGONS_TO_REPAIR] = 0;
+            data.meshStats[N_POLYGONS_ADDED_AFTER_REPAIR] = 0;
+            data.meshStats[N_VERTICES] = 0;
+            data.meshStats[N_EDGES] = 0;
+            data.meshStats[N_BORDER_EDGES] = 0;
             
-            timeStats[T_TRIANGULATION_GENERATION] = 0.0;
-            timeStats[T_LABEL_MAX_EDGES] = 0.0;
-            timeStats[T_LABEL_FRONTIER_EDGES] = 0.0;
-            timeStats[T_LABEL_SEED_EDGES] = 0.0;
-            timeStats[T_TRAVERSAL_AND_REPAIR] = 0.0;
-            timeStats[T_TRAVERSAL] = 0.0;
-            timeStats[T_REPAIR] = 0.0;
+            data.timeStats[T_TRIANGULATION_GENERATION] = 0.0;
+            data.timeStats[T_LABEL_MAX_EDGES] = 0.0;
+            data.timeStats[T_LABEL_FRONTIER_EDGES] = 0.0;
+            data.timeStats[T_LABEL_SEED_EDGES] = 0.0;
+            data.timeStats[T_TRAVERSAL_AND_REPAIR] = 0.0;
+            data.timeStats[T_TRAVERSAL] = 0.0;
+            data.timeStats[T_REPAIR] = 0.0;
         }
-
+        std::vector<OutputIndex>& getOutputSeeds() override {
+            return data.outputSeeds;
+        }
         MeshType* refineMesh(const MeshType* inputMesh) override;
+        std::unordered_map<MeshStat,int>& getRefinementStats() override {
+            return data.meshStats;
+        }
+        std::unordered_map<TimeStat,double>& getRefinementTimes() override {
+            return data.timeStats;
+        }
 };
 
 #include <mesh_refiners/polylla/polylla_refiner.ipp>
