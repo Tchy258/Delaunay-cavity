742d57799b7b087dd49ccbb1b6fe33aec2e6fbac
Tchy258 <tchy258@yahoo.cl>
2025-10-06 20:59:23 -0300

    feat: triangle comparator concept to sort triangles

---
+++ include/concepts/triangle_comparator.hpp
@@ -0,0 +1,18 @@
+#ifndef TRIANGLE_COMPARATOR_HPP
+#define TRIANGLE_COMPARATOR_HPP
+#include <concepts>
+#include <vector>
+#include <concepts/mesh_data.hpp>
+
+template <typename Comparator, typename Mesh>
+inline constexpr bool isNullComparator = false;
+
+template <typename Comparator, typename Mesh>
+concept TriangleComparatorBase = requires(const Mesh* meshptr, const typename Mesh::FaceIndex& t1, const typename Mesh::FaceIndex& t2) {
+    {Comparator::compare(meshptr,t1,t2)} -> std::same_as<bool>;
+} && MeshData<Mesh>;
+
+template <typename Comparator, typename Mesh>
+concept TriangleComparator = TriangleComparatorBase<Comparator, Mesh> || isNullComparator<Comparator,Mesh>;
+
+#endif // TRIANGLE_COMPARATOR_HPP
