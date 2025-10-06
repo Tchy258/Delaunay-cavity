6887d6b2a3838311162b79f2334a29e87ea30b17
Tchy258 <tchy258@yahoo.cl>
2025-10-06 20:59:09 -0300

    feat: null refinement criterion and tag

--- include/concepts/refinement_criterion.hpp
+++ include/concepts/refinement_criterion.hpp
@@ -3,15 +3,24 @@
 #include <concepts>
 #include <concepts/mesh_data.hpp>
 
-/**
- * A refinement criterion must apply to a particular mesh that conforms to the MeshData concept,
- * and it must have an operator() that receives an input mesh, a polygon index, and returns true or false
- */
+template <typename Criterion, MeshData Mesh>
+inline constexpr bool isNullRefinementCriterion = false;
+
 template <typename Criterion, typename Mesh>
-concept RefinementCriterion = requires(Criterion criterion, Mesh inputMesh, int polygonIndex) {
+concept RefinementCriterionBase = requires(Criterion criterion, const Mesh* inputMesh, typename Mesh::FaceIndex polygonIndex) {
     {criterion(inputMesh,polygonIndex)} -> std::convertible_to<bool>;
 } &&
 MeshData<Mesh>;
 
+
+/**
+ * A refinement criterion must apply to a particular mesh that conforms to the MeshData concept,
+ * and it must have an operator() that receives an input mesh, a polygon index, and returns true or false
+ * 
+ * The NullRefinementCriterion defines a partial specialization of isNullRefinementCriterion to skip
+ * refining altogether
+ */
+template <typename Criterion, typename Mesh>
+concept RefinementCriterion = RefinementCriterionBase<Criterion,Mesh> || isNullRefinementCriterion<Criterion,Mesh>;
 
 #endif
